5ee74811c65649cd7a817c76f6d9f70045deac63
Eyrkki <78674053+EyrkkiSkye@users.noreply.github.com>
2022-01-05 00:42:08 -0500

    Create weaken.js

---
+++ src/weaken.js
@@ -0,0 +1,15 @@
+export async function main(ns) {
+  const target = ns.args[0]
+  const threads = ns.args[1]
+  const delay = ns.args[2]
+
+  if (delay && delay > 0) {
+    await ns.asleep(delay)
+  }
+
+  ns.print(`Starting operation: weaken on ${target} in ${threads} threads`)
+  await ns.weaken(target, { threads, stock: true })
+  ns.exit()
+}
+
+// vim: set ft=javascript :
