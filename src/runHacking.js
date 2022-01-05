321b2071a07e96677ec079abbb325cfe0172b71a
Eyrkki <78674053+EyrkkiSkye@users.noreply.github.com>
2022-01-05 01:13:48 -0500

    Update runHacking.js

--- src/runHacking.js
+++ src/runHacking.js
@@ -13,13 +13,13 @@ export async function main(ns) {
 
   if (homeRam >= 32) {
     ns.tprint(`[${localeHHMMSS()}] Spawning spider.ns`)
-    await ns.run('spider.ns', 1, 'mainHack.ns')
+    await ns.run('spider.ns', 1, 'mainHack.js')
     await ns.asleep(1000)
     //ns.tprint(`[${localeHHMMSS()}] Spawning playerServers.ns`)
     //ns.spawn('playerServers.ns', 1)
   } else {
     ns.tprint(`[${localeHHMMSS()}] Spawning spider.ns`)
-    ns.spawn('spider.ns', 1, 'mainHack.ns')
+    ns.spawn('spider.ns', 1, 'mainHack.js')
   }
 }
 
