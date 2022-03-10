bf32ba265af084f52c9ab84952438227e6dde01a
Harm Endres <harm.mueller@gmail.com>
2022-03-10 07:17:15 +0100

    added script to sell hacknet hashes

---
+++ src/sellHashes.js
@@ -0,0 +1,26 @@
+/** @param {NS} ns **/
+import { settings, getItem, setItem, localeHHMMSS, createUUID } from 'common.js'
+
+
+export async function main(ns) {
+  ns.tprint(`[${localeHHMMSS()}] Starting playerServers.js`)
+
+  let hostname = ns.getHostname()
+  if (hostname !== 'home') {
+    throw new Exception('Run the script from home')
+  }
+
+  let keep_percent = 0.5
+
+  while (true) {
+    let capacity = ns.hacknet.hashCapacity()
+    while (ns.hacknet.numHashes() / capacity > keep_percent) {
+      ns.hacknet.spendHashes('Sell for Money')
+      await ns.asleep(1)
+    }
+
+    await ns.asleep(5000)
+  }
+}
+
+// vim: set ft=javascript :
