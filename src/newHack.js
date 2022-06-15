23d829bec0724d043fc7486ce6a34361e5516f5c
sychris <sychris9@gmail.com>
2022-06-15 01:44:54 -0400

    removed no longer used variables

--- src/newHack.js
+++
@@ -1,7 +0,0 @@
-function getCyclesToFullyHack(ns, hostname, servers) {
-    if (ns.fileExists("formulas.exe", "home")) {
-        return  Math.ceil((settings().maxServerValueToHack / 100) /  ns.formulas.hacking.hackPercent(ns.getServer(hostname),ns.getPlayer()))
-    } else {
-        return Math.ceil(ns.hackAnalyzeThreads(hostname, servers[hostname].maxMoney * (settings().maxServerValueToHack / 100)))
-    }
-}
